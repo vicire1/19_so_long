68137b8d8f49a882a1cc26848a6ff53f6cc4e676
Victor Decleire <vdecleir@fu-r1-p5.s19.be>
2023-12-08 17:07:51 +0100

    r

---
+++ so_long.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vdecleir <vdecleir@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/08 16:56:03 by vdecleir          #+#    #+#             */
+/*   Updated: 2023/12/08 17:03:29 by vdecleir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <math.h>
+
+#endif
